f57fce559bc9c717c7fb6ca0594a4c7e1536d2bf
Junior Corman <junior.corman@gmail.com>
2016-12-21 01:37:23 -0500

    add String example

--- hello.cpp
+++ hello.cpp
@@ -2,6 +2,7 @@
 #include <ctime>
 #include <cstdlib>
 #include <iomanip>
+#include <cstring>
 
 using namespace std;
 using std::setw;
@@ -99,6 +100,28 @@ int main() {
 	for ( int j = 0; j < 10; j++ ) {
 		cout << setw( 7 )<< j << setw( 13 ) << n[ j ] << endl;
 	}
+	
+	// Strings
+	char greeting[6] = {'H', 'e', 'l', 'l', 'o', '\0'};
+	cout << "Greeting message: ";
+	cout << greeting << endl;
+	
+	char str1[10] = "Hello";
+	char str2[10] = "World";
+	char str3[10];
+	int  len ;
+
+	// copy str1 into str3
+	strcpy( str3, str1);
+	cout << "strcpy( str3, str1) : " << str3 << endl;
+
+	// concatenates str1 and str2
+	strcat( str1, str2);
+	cout << "strcat( str1, str2): " << str1 << endl;
+
+	// total lenghth of str1 after concatenation
+	len = strlen(str1);
+	cout << "strlen(str1) : " << len << endl;
 	
 	return 0;
 }
