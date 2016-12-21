d418c8de7cf719121221650b7732fd3c456eb573
Junior Corman <junior.corman@gmail.com>
2016-12-21 01:45:59 -0500

    Add Pointer example

--- hello.cpp
+++ hello.cpp
@@ -123,6 +123,33 @@ int main() {
 	len = strlen(str1);
 	cout << "strlen(str1) : " << len << endl;
 	
+	// Pointers
+	
+	int  var1;
+   	char var2[10];
+
+   	cout << "Address of var1 variable: ";
+	cout << &var1 << endl;
+
+	cout << "Address of var2 variable: ";
+	cout << &var2 << endl;
+	
+	int  var = 20;   // actual variable declaration.
+	int  *ip;        // pointer variable 
+
+	ip = &var;       // store address of var in pointer variable
+
+	cout << "Value of var variable: ";
+	cout << var << endl;
+
+	// print the address stored in ip pointer variable
+	cout << "Address stored in ip variable: ";
+	cout << ip << endl;
+
+	// access the value at the address available in pointer
+	cout << "Value of *ip variable: ";
+	cout << *ip << endl;
+		
 	return 0;
 }
 
